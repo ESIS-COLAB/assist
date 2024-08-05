6c03f18d5f0019e20315c49d7ea251b1f4df7499
Piero <pgarciam@gmail.com>
2024-08-04 23:13:53 -0500

    Agregando archivos funciones.h y Registros.

--- include/funciones.h
+++ include/funciones.h
@@ -1,12 +1,51 @@
-// funciones.h
-#ifndef FUNCIONES_H 
+#ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-// Aqui ira el INDICE de funciones a utilizar en main.cpp
-// Las modificaciones de cada funciones se realizara en src
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <json.hpp>
+#include <iomanip>
+#include <vector>
 
-void saludar();
-int sumar(int a, int b, int c);
-int multiplicar(int a, int b, int c); 
+using namespace std;
 
-#endif // FUNCIONES_H
+// Estructura para representar un alumno
+struct Alumno {
+    string nombre;
+    string codigo;
+    bool asistencia;
+    bool tardanza;
+};
+
+// Estructura para representar una sesion de clases
+struct Sesion {
+    int numero;
+    vector<Alumno> asistencia;
+};
+
+// Estructura para representar una clase
+struct Clase {
+    string nombre;
+    string aula;
+    vector<Sesion> sesiones;
+};
+
+// Estructura para representar un aula
+struct Aula {
+    string nombre;
+    vector<Alumno> alumnos;
+};
+
+
+void registrarAula(vector<Aula>& aulas);
+void registrarClase(vector<Clase>& clases, vector<Aula>& aulas);
+void registrarSesion(Clase& clase, vector<Aula>& aulas);
+void visualizarAsistencia(Clase& clase, int sesion);
+void modificarAsistencia(Clase& clase, int sesion, string nombreAlumno);
+void guardarDatos(const vector<Aula>& aulas, const vector<Clase>& clases);
+void cargarDatos(vector<Aula>& aulas, vector<Clase>& clases);
+void calcularPorcentajeAsistencia(Clase& clase);
+
+#endif  // FUNCIONES_H
